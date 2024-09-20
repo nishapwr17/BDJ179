97d52e004bd36d4a1a0dd67459ada1721e4b2598
Nisha <Nisha@DELL>
2024-09-20 15:09:36 +0530

    by nisha changes done

--- Fdj179/src/com/HomeController.java
+++ Fdj179/src/com/HomeController.java
@@ -1,6 +1,10 @@
 package com;
 
 public class HomeController {
+	public void m1(){
+		System.out.println("method m1 call for demo project");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("welcome");
 	}
